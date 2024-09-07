8dd25a9d5fab200663a24761e2531c0c42d142b7
0jkhine <0jkhine@tawacollege.school.nz>
2024-09-07 14:25:30 +1200

    Add files via upload

---
+++ contact_v1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Contact Us</title>
+  <meta name="description" content="Contact page for AudioOcean" />
+  <meta name="keywords" content="contact, audio ocean, music" />
+  <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+  <link rel="stylesheet" type="text/css" href="style/style.css" title="style" />
+</head>
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="logo">
+        <div id="logo_text">
+          <h1><a href="index.html">Banner</a></h1>
+        </div>
+      </div>
+    </div>
+    <div id="menubar">
+      <ul id="menu">
+        <li class="selected"><a href="index.html">Home</a></li>
+        <li><a href="database.php">Database</a></li>
+        <li><a href="history.html">History</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+      </div>
+    </div>
+    <div id="site_content">
+      <div class="sidebar">
+      </div>
+      <div id="content">
+        <h1>Contact Us</h1>
+        <form action="" method="post">
+          <div class="form_settings">
+            <p><span>Name</span><input class="contact" type="text" name="Name" required /></p>
+            <p><span>Email Address</span><input class="contact" type="email" name="Email" required /></p>
+            <p><span>Phone</span><input class="contact" type="tel" name="Phone" /></p>
+            <p><span>Message</span><textarea class="contact textarea" rows="8" cols="50" name="Message" required></textarea></p>
+            <p style="padding-top: 15px"><span>&nbsp;</span><input class="submit" type="submit" value="Submit" /></p>
+          </div>
+        </form>
+      </div>
+    </div>
+    <div id="footer">
+      Copyright &copy; Jeremy Khine 2024 all rights reserved
+    </div>
+  </div>
+</body>
+</html>
